495e615dc766df6689a09be4e0705b7a9bfe1760
coelhosophia <coelhosophia1608@gmail.com>
2026-06-24 11:02:08 -0300

    tela de pacientes e modo claro

--- views/patients_view.py
+++ views/patients_view.py
@@ -37,7 +37,7 @@ class PatientsView(ctk.CTkFrame):
 
         ctk.CTkLabel(
             header,
-            text="👨‍⚕️ Pacientes",
+            text=" Pacientes",
             font=ctk.CTkFont(size=28, weight="bold"),
             text_color=get_color("text"),
         ).pack(side="left", padx=20, pady=15)
